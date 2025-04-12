2896f58813c117d96e00604c1c2b78c001c0b8d9
Discovertsy AI <test@discovertsy.com>
2025-04-12 17:30:32 +0200

    refactor: migrate Toast component to functional component

--- src/components/Toast.js
+++ src/components/Toast.js
@@ -1,37 +1,31 @@
-import React, { Component, Fragment } from "react";
-
+import React, { Fragment } from "react";
 import MySnackbarContent from "./MySnackbarContent";
-
 //material ui
 import { withStyles } from "@material-ui/core/styles";
 import Snackbar from "@material-ui/core/Snackbar";
 
-// This example assumes you have a way to know/load this information
-
 const styles = theme => ({});
 
-class Toast extends Component {
-  // Render your list
-  render() {
-    return (
-      <Fragment>
-        <Snackbar
-          anchorOrigin={{
-            vertical: "bottom",
-            horizontal: "left"
-          }}
-          open={this.props.open}
-          autoHideDuration={6000}
-          onClose={this.props.handleClose}
-        >
-          <MySnackbarContent
-            onClose={this.props.handleClose}
-            variant={this.props.type}
-            message={this.props.message}
-          />
-        </Snackbar>
-      </Fragment>
-    );
-  }
+function Toast(props) {
+  return (
+    <Fragment>
+      <Snackbar
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "left"
+        }}
+        open={props.open}
+        autoHideDuration={6000}
+        onClose={props.handleClose}
+      >
+        <MySnackbarContent
+          onClose={props.handleClose}
+          variant={props.type}
+          message={props.message}
+        />
+      </Snackbar>
+    </Fragment>
+  );
 }
+
 export default withStyles(styles, { withTheme: true })(Toast);

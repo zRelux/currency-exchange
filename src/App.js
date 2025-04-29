b26ab88efb874414f19e933ddcb96bf9c7f3fd58
Discovertsy AI <test@discovertsy.com>
2025-04-29 22:25:20 +0200

    refactor: remove Material UI and add placeholder for Shadcn equivalent in App.js

--- src/App.js
+++ src/App.js
@@ -1,30 +1,12 @@
 import React, { Fragment } from "react";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import { createMuiTheme, MuiThemeProvider } from "@material-ui/core/styles";
 import "./App.css";
 import Converion from "./components/Conversion.tsx";
-
-const theme = createMuiTheme({
-  typography: {
-    useNextVariants: true
-  },
-  palette: {
-    primary: {
-      main: "#3f51b4"
-    },
-    secondary: {
-      main: "#00796b"
-    }
-  }
-});
 
 function App() {
   return (
     <Fragment>
-      <CssBaseline />
-      <MuiThemeProvider theme={theme}>
-        <Converion />
-      </MuiThemeProvider>
+      {/* Shadcn equivalent of CssBaseline */}
+      <Converion />
     </Fragment>
   );
 }
